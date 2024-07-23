15b0286e5cfebec15b1723d4f535b713b3ae95fe
cami benitez <camila.benitez0021@gmail.com>
2024-07-22 22:53:31 -0400

    add: pues aqui trabajando la recuperacion de valores y el evento del boton, por supuesto, usando click.

--- js/app.js
+++ js/app.js
@@ -5,9 +5,22 @@ se cargue TODO el documento HTML, por ultimo javascript
 document
 .addEventListener("DOMContentLoaded", function(){
     alert("Holis");
+    // esta es zona segura
+    //console.log(document.getElementById("txtNombre").value);
+    //console.log(document.getElementById("txtApellido").value)
+});
+// añadiendo evento clic al botón mostrar
+const btnMostrar = document.getElementById("btnMostrar");
+btnMostrar.addEventListener("click", function() {
+    //console.log('haciendo click');
+    //alert("hice clic");
+    //recuperando info del formulario
+    const nombres = document.getElementById("txtNombre").value;
+    const apellido = document.getElementById("txtApellido").value;
+    const cedula = document.getElementById("txtCedula").value;
+    const direccion = document.getElementById("txtDireccion").value;
+    console.log('nombres ' +nombres+ ', apellidos ' +apellido+ ', cedula ' + cedula+ ',direccion' + direccion);
+    alert('nombres ' +nombres+ ', apellidos ' +apellido+ ', cedula ' + cedula+ ',direccion' + direccion);
 });
 
-function hola(nombre, apellido) {
-    console.log(nombre, apellido);
-    // codigo reutilizable 
-}
+
